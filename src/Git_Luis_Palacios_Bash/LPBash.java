021b7094c2c2b95bf3fbf6b15dfacddc0542e660
Luisjal <luispalaciosiglesias@gmail.com>
2022-11-09 10:55:39 +0100

    Modificacion-5

--- src/Git_Luis_Palacios_Bash/LPBash.java
+++ src/Git_Luis_Palacios_Bash/LPBash.java
@@ -4,6 +4,7 @@ public class LPBash {
 	public static void main(String[] args) {
 		System.out.println("Hola hola pepsicola 1");
 		System.out.println("Hola hola pepsicola 2");
+		System.out.println("Hola hola pepsicola 5");
 
 	}
 
